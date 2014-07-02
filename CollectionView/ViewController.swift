c7a0ac943de3588c863102459299b0898201d7a0
netwire88 <netwire88@gmail.com>
2014-07-02 14:59:29 -0400

    Working version in Swift

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var imagesArray:String[] = []
+    let cellIdentifier = "CollectionCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagesArray += "Swift"
+        imagesArray += "is"
+        imagesArray += "the"
+        imagesArray += "next"
+        imagesArray += "hotness"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView!) -> Int {
+        return 1
+    }
 
-
+    func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int {
+        return imagesArray.count
+    }
+    
+    func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell! {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+        var label = cell.viewWithTag(100) as UILabel
+        label.text = imagesArray[indexPath.row]
+        cell.layer.borderWidth = 2.0
+        cell.layer.borderColor = UIColor.whiteColor().CGColor
+        cell.layer.cornerRadius = 50.0
+        
+        return cell
+    }
+    
 }
 
